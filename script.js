02126e196d8a73a9b164344ff8299f91185cef34
Arthur <doudiat1998@gmail.com>
2025-06-01 00:11:26 +0300

    added logic to the rock paper scissor game

--- script.js
+++ script.js
@@ -1,1 +1,85 @@
-console.log("hello world");
+playGame();
+
+
+
+
+
+
+
+
+
+function playGame()
+{
+    let score = {
+        humanScore:0,
+        ComputerScore:0
+    }
+
+    for (let round = 0; round < 5; round++) {
+        const computerChoice = getComputerChoice();
+        //console.log(computerChoice);
+        const humanChoice = getHumanChoice();
+        playRound(humanChoice, computerChoice,score);
+    }
+    if(score.humanScore>score.ComputerScore)
+        console.log("Human wins "+score.humanScore+" to "+score.ComputerScore);
+    else if(score.ComputerScore>score.humanScore)
+        console.log("Computer wins "+score.ComputerScore+" to " +score.humanScore);
+    else
+        console.log("It's a tie "+score.humanScore+" to "+score.ComputerScore);
+
+
+}
+
+
+function playRound(humanChoice, computerChoice,score) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice == "rock" && computerChoice == "scissor") {
+        score.humanScore++;
+        console.log("Human wins!");
+    }
+    else if (humanChoice == "paper" && computerChoice == "rock") {
+        score.humanScore++;
+        console.log("Human wins!");
+    }
+    else if (humanChoice == "scissor" && computerChoice == "paper") {
+        score.humanScore++;
+        console.log("Human wins!");
+    }
+    else if (humanChoice == computerChoice) {
+        console.log("It's a tie!");
+    }
+    else {
+        score.ComputerScore++;
+        console.log("Computer wins!");
+    }
+}
+
+function getHumanChoice() {
+    return prompt("Enter rock,paper or scissor?: ");
+}
+
+
+function getComputerChoice() {
+    let guess = getRandomIntegearInclusive(0, 2);
+    //rock:0 paper:1 scissor:2
+    let result;
+    switch (guess) {
+        case 0:
+            result = "rock";
+            break;
+        case 1:
+            result = "paper";
+            break;
+        case 2:
+            result = "scissor";
+            break;
+    }
+    return result;
+}
+
+function getRandomIntegearInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
